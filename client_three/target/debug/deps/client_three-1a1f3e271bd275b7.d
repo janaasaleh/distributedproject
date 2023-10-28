4e3f8cf00f01dc27de9da1684e1970e5c224cfe8
adhamzorkani <adham.zorkani@aucegypt.edu>
2023-10-28 19:17:23 +0200

    .

---
+++ client_three/target/debug/deps/client_three-1a1f3e271bd275b7.d
@@ -0,0 +1,5 @@
+/mnt/Doma/AUC/Distributed systems/project/distributedproject/client_three/target/debug/deps/client_three-1a1f3e271bd275b7.rmeta: src/main.rs
+
+/mnt/Doma/AUC/Distributed systems/project/distributedproject/client_three/target/debug/deps/client_three-1a1f3e271bd275b7.d: src/main.rs
+
+src/main.rs:
---
+++ client_three/target/debug/deps/linux_raw_sys-47d8adb2a0a757ca.d
@@ -0,0 +1,8 @@
+/mnt/Doma/AUC/Distributed systems/project/distributedproject/client_three/target/debug/deps/linux_raw_sys-47d8adb2a0a757ca.rmeta: /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/lib.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/errno.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/general.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/ioctl.rs
+
+/mnt/Doma/AUC/Distributed systems/project/distributedproject/client_three/target/debug/deps/linux_raw_sys-47d8adb2a0a757ca.d: /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/lib.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/errno.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/general.rs /home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/ioctl.rs
+
+/home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/lib.rs:
+/home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/errno.rs:
+/home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/general.rs:
+/home/adham/.cargo/registry/src/index.crates.io-6f17d22bba15001f/linux-raw-sys-0.3.8/src/x86_64/ioctl.rs:
